4ee2751da29c28d6f34ac6ff29f3d60b1107133c
Your Name <you@example.com>
2023-10-28 20:15:37 -0400

    rework and format

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,31 +10,27 @@ const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-
-const transcriptionRoutes = require('./routes/transcription');
-const chatGptRoutes = require('./utils/chatgpt');
-
+const transcriptionRoutes = require("./routes/transcription");
+const chatGptRoutes = require("./utils/chatgpt");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.use("/transcription", transcriptionRoutes);
+app.use("/chatgpt", chatGptRoutes);
 
-app.use('/transcription', transcriptionRoutes);
-app.use('/chatgpt', chatGptRoutes);
-
-
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 server.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
--- utils/dynamodb.js
+++ utils/dynamodb.js
@@ -1,51 +1,55 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 // Configure AWS
 AWS.config.update({
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const saveTranscriptionToDB  = (transcription, summary) => {
-    const params = {
-        TableName: 'Transcriptions',  
-        Item: {
-          id: Date.now().toString(), // unique identifier, using timestamp for simplicity
-          transcription: transcription
-        }
-      };
+const saveTranscriptionToDB = (uid, transcription, summary) => {
+  const params = {
+    TableName: "Transcriptions",
+    Item: {
+      id: uid,
+      noteID: Date.now().toString(), // unique identifier, using timestamp for simplicity
+      transcription: transcription,
+    },
+  };
 
-    docClient.put(params, (err, data) => {
-        if (err) {
-            console.error("Error saving to DynamoDB", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Saved successfully:", JSON.stringify(data, null, 2));
-        }
-    });
+  docClient.put(params, (err, data) => {
+    if (err) {
+      console.error("Error saving to DynamoDB", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Saved successfully:", JSON.stringify(data, null, 2));
+    }
+  });
 };
 
 const updateTranscriptionInDB = async (id, updatedTranscription) => {
-    const params = {
-        TableName: "Transcriptions",
-        Key: {
-            "id": id
-        },
-        UpdateExpression: "set transcription = :t",
-        ExpressionAttributeValues: {
-            ":t": updatedTranscription
-        },
-        ReturnValues: "UPDATED_NEW"
-    };
+  const params = {
+    TableName: "Transcriptions",
+    Key: {
+      id: id,
+    },
+    UpdateExpression: "set transcription = :t",
+    ExpressionAttributeValues: {
+      ":t": updatedTranscription,
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
 
-    try {
-        await docClient.update(params).promise();
-    } catch (err) {
-        console.error("Unable to update item. Error:", JSON.stringify(err, null, 2));
-        throw err;
-    }
+  try {
+    await docClient.update(params).promise();
+  } catch (err) {
+    console.error(
+      "Unable to update item. Error:",
+      JSON.stringify(err, null, 2)
+    );
+    throw err;
+  }
 };
 
-module.exports = { saveTranscriptionToDB , updateTranscriptionInDB, docClient };
+module.exports = { saveTranscriptionToDB, updateTranscriptionInDB, docClient };
